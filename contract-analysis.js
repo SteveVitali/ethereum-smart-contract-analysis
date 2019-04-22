2c83608a5b0aff638bddcaf42921edd4c3f7bbc7
Steven Vitali <14stevevitali@gmail.com>
2019-04-22 00:03:32 -0400

    Add line-by-line CSV reader boilerplate

---
+++ contract-analysis.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import readLine from 'readline';
+import Web3 from 'web3';
+
+const options = {};
+const web3 = new Web3('http://localhost:8545', null, options);
+
+const INPUT_CSV_PATH = '../example-csvs/logs_07500000_07532178.csv';
+const JSON_CACHE_PATH = '../analysis-data/json-cache.json';
+
+const dataStream = fs.createReadStream(INPUT_CSV_PATH);
+const lineReader = readLine.createInterface(dataStream);
+
+const cacheStream = fs.createWriteStream(JSON_CACHE_PATH);
+
+const startTime = new Date();
+const secondsSince = () => (new Date() - startTime) / 1000;
+
+let lineCount = 0;
+
+lineReader.on('line', (line) => {
+  // process line
+  lineCount += 1;
+  if (lineCount % 10000 === 0) {
+  	console.log('Processed', lineCount, 'in', secondsSince() + 's');
+  }
+});
+
+lineReader.on('close', () => {
+  cacheStream.end();
+
+  // On local machine: Processed 2682461 in 281.728s
+  //   with file `logs_07500000_07532178`
+  //   ~10,000 lines per second
+  console.log('Processed', lineCount, 'in', secondsSince() + 's');
+});
