72bcf3580c4a8565b24796d0192426f51e7438d3
Steven Vitali <14stevevitali@gmail.com>
2019-04-22 10:03:23 +0000

    Update contract-analysis test for ec2

--- contract-analysis.js
+++ contract-analysis.js
@@ -1,12 +1,16 @@
-import fs from 'fs';
-import readLine from 'readline';
-import Web3 from 'web3';
+// import fs from 'fs';
+// import readLine from 'readline';
+// import Web3 from 'web3';
+
+const fs = require('fs');
+const readLine = require('readline');
+const Web3 = require('web3');
 
 const GETH_URL = 'http://localhost:8545';
 const web3 = new Web3(new Web3.providers.HttpProvider(GETH_URL));
 
-const INPUT_CSV_PATH = '../example-csvs/logs_07500000_07532178.csv';
-const JSON_CACHE_PATH = '../analysis-data/json-cache.json';
+const INPUT_CSV_PATH = './ethereumetl/export/logs/start_block=07500000/end_block=07532178/logs_07500000_07532178.csv';
+const JSON_CACHE_PATH = './analysis-data/json-cache.json';
 
 const dataStream = fs.createReadStream(INPUT_CSV_PATH);
 const lineReader = readLine.createInterface(dataStream);
@@ -30,7 +34,10 @@ lineReader.on('close', () => {
   cacheStream.end();
 
   // On local machine: Processed 2682461 in 281.728s
-  //   with file `logs_07500000_07532178`
+  //   with file `logs_07500000_07532178` (~1.3GB)
   //   ~10,000 lines per second
+  // On EC2 t2.large: Processed 2682461 in 46.948s
+  //   57k lines/sec; ~36sec/GB ==> ~4.5hr for 450
   console.log('Processed', lineCount, 'in', secondsSince() + 's');
 });
+
