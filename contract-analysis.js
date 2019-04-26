755ab6a3aed693290acb22625ab840e1eec86a4e
Steven Vitali <14stevevitali@gmail.com>
2019-04-26 01:24:26 -0400

    Add line-by-line module

--- contract-analysis.js
+++ contract-analysis.js
@@ -1,12 +1,14 @@
-import fs from 'fs';
-import readLine from 'readline';
-import Web3 from 'web3';
+const fs = require('fs');
+const readLine = require('readline');
+const Web3 = require('web3');
 
 const GETH_URL = 'http://localhost:8545';
 const web3 = new Web3(new Web3.providers.HttpProvider(GETH_URL));
 
 const INPUT_CSV_PATH = '../example-csvs/logs_07500000_07532178.csv';
 const JSON_CACHE_PATH = '../analysis-data/json-cache.json';
+
+const LOG_EACH = 1;
 
 const dataStream = fs.createReadStream(INPUT_CSV_PATH);
 const lineReader = readLine.createInterface(dataStream);
@@ -20,9 +22,10 @@ let lineCount = 0;
 
 lineReader.on('line', (line) => {
   // process line
+
   lineCount += 1;
-  if (lineCount % 10000 === 0) {
-  	console.log('Processed', lineCount, 'in', secondsSince() + 's');
+  if (lineCount % LOG_EACH === 0) {
+    console.log('Processed', lineCount, 'in', secondsSince() + 's');
   }
 });
 
