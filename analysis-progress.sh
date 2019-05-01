46d38c202d6b555104a52f47719cdf4a920fb8f9
Steven Vitali <14stevevitali@gmail.com>
2019-05-01 05:48:55 +0000

    Update analysis-progress to print all contract counts in job block range

--- analysis-progress.sh
+++ analysis-progress.sh
@@ -7,16 +7,20 @@ ls ethereumetl/export/contracts_analysis
 
 echo "Analyzing range: ${START_BLOCK} - ${END_BLOCK}"
 
-# Default batch size = 10000
-batch_size=${BATCH_SIZE:=10000}
+# Default batch size = 100000
+batch_size=${BATCH_SIZE:=100000}
 
 for (( s=$START_BLOCK; s <= $END_BLOCK; s+=$batch_size )); do
-	e=(s + batch_size - 1)
+	e=$(($s + $batch_size - 1))
+
 	START=start_block=0${s}
 	END=end_block=0${e}
 	CSV=contracts_0${s}_0${e}.csv
 
-	echo "Num Contracts in ${s}-${e}:"
-	wc -l ethereumetl/export/contracts/${START}/${END}/${CSV}
+        contract_path=ethereumetl/export/contracts/${START}/${END}/${CSV}
 
+        contract_count=$(wc -l $contract_path | awk '{print $1;}')
+
+	echo "# Contracts in ${s}-${e}: ${contract_count}"
 done
+
