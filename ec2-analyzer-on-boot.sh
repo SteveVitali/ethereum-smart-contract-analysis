9bf944255d9265bed7c50861ce0fe7ff49de900d
Steven Vitali <14stevevitali@gmail.com>
2019-05-01 00:06:58 -0400

    Update ec2 analyzer boot script

--- ec2-analyzer-on-boot.sh
+++ ec2-analyzer-on-boot.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-cd ethereum-smart-contract-analysis/
-
 pip install --upgrade pip setuptools
 
+# Python virtuel env secret sauce to make Oyente work with web3
 python -m virtualenv env
 source env/bin/activate
-
 python3 -m venv venv
 . venv/bin/activate
 
 pip install web3
 
+# Set node version to 10.x
 nvm use 10.15.3
+
+# Pull the latest from git remote
+git pull
+
+# Sync up with S3
+bash ./push-pull-s3.sh
